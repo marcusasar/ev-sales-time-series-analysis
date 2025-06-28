4f8687d9ee47c33c11d417b423d197e0a40d3386
Marcus Asare <165590698+marcusasar@users.noreply.github.com>
2025-06-28 11:52:57 +0000

    Update 02_global_ev_yoy_growth_rate.sql

--- project_sql_queries/02_global_ev_yoy_growth_rate.sql
+++ project_sql_queries/02_global_ev_yoy_growth_rate.sql
@@ -1,7 +1,11 @@
+-- Calculate the year-over-year (YoY) percentage growth in global EV sales.
+-- This query helps assess whether EV sales are accelerating consistently each year.
+-- It uses the LAG() function to compare each year's sales to the previous year.
+
 SELECT
 	year,
 	sales,
-	ROUND((sales / LAG(sales) OVER(ORDER BY year)-1)*100,2) AS yoy_pct_growth_
+	ROUND((sales / LAG(sales) OVER(ORDER BY year)-1)*100,2) AS yoy_pct_growth
 FROM
 (
 	SELECT 
